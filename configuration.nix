994cb8589bf4b010977467c441aedad9d49c5f47
pietrodito <pietrodito@gmail.com>
2020-03-15 17:12:46 +0100

    Network manager conflicts

--- configuration.nix
+++ configuration.nix
@@ -16,7 +16,7 @@
 
   networking.networkmanager.enable = true;
   networking.hostName = "nixos.pierre.cerim"; # Define your hostname.
-  networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
 
   # The global useDHCP flag is deprecated, therefore explicitly set to false here.
   # Per-interface useDHCP will be mandatory in the future, so this generated config
